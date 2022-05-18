9ceba98322b7984ac03b4b98c9dbe3de6eab3c2c
ismail nbiba <ismailnbiba@gmail.com>
2022-05-18 14:38:57 +0100

    webappPy

---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+
+
+class AddTaskForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    submit= SubmitField('submit')
+
+
+class DeleteTaskForm(FlaskForm) :
+    submit = SubmitField('Delete')
+    
+
+
